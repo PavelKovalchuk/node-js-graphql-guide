53c4b25dbe83ec8de4d37f14a6b2576b49fc643b
PavelKovalchuk <kovpavanat@gmail.com>
2019-10-14 11:42:44 +0300

    418/ handle errors

--- app.js
+++ app.js
@@ -62,6 +62,19 @@ app.use("/graphql", graphqlHttp({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
   graphiql: true,
+  formatError(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || "An error occured!";
+    const code = err.originalError.code || 500;
+    return {
+      message: message,
+      status: code,
+      data: data,
+    };
+  }
 }));
 
 app.use((error, req, res, next) => {
