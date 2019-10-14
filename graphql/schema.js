4686be3bd93b0c884e387554326f1dd66cc34b28
PavelKovalchuk <kovpavanat@gmail.com>
2019-10-14 11:01:21 +0300

    416/ add createUser mutation

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,14 +1,42 @@
 const {buildSchema} = require("graphql");
 
-module.exports = buildSchema(`
-  type TestData {
-    text: String!
-    views: Int!
+module.exports = buildSchema(` 
+
+  type Post {
+     _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
   }
+  
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+    posts: [Post!]!
+  }
+
+  input UserInputData {
+    email: String!
+    name: String!
+    password: String!
+  }
+  
+  type RootMutation {
+    createUser(userInput: UserInputData): User!
+  }
+  
   type RootQuery {
-    hello: TestData!
+    hello: String
   }
+  
   schema {
-    query : RootQuery
+    query: RootQuery
+    mutation : RootMutation
   }
 `);
